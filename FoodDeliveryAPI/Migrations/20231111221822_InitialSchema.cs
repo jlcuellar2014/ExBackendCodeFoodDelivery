62f5922d337bbf7a10ae156564b2ff0ba7aa26b5
jlcuellar2014 <35740606+jlcuellar2014@users.noreply.github.com>
2023-11-11 23:30:56 +0100

    Adding test datasets and dropping the indexes created by ef for the columns ProductId and OrderId on the entity OrderProducts

---
+++ FoodDeliveryAPI/Migrations/20231111221822_InitialSchema.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodDeliveryAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CustomerName = table.Column<string>(type: "TEXT", nullable: false),
+                    ReceivingAddress = table.Column<string>(type: "TEXT", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DeliveryVehicles",
+                columns: table => new
+                {
+                    DeliveryVehicleId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DeliverymanName = table.Column<string>(type: "TEXT", nullable: false),
+                    DeliverymanPhoneNumber = table.Column<string>(type: "TEXT", nullable: false),
+                    VehicleDescription = table.Column<string>(type: "TEXT", nullable: false),
+                    Coordinate = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeliveryVehicles", x => x.DeliveryVehicleId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Restaurants",
+                columns: table => new
+                {
+                    RestaurantId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    RestaurantName = table.Column<string>(type: "TEXT", nullable: false),
+                    PickUpAddress = table.Column<string>(type: "TEXT", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "TEXT", nullable: false),
+                    Coordinate = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restaurants", x => x.RestaurantId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DeliveryVehicleCoordinates",
+                columns: table => new
+                {
+                    DeliveryVehicleCoordinatesId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DeliveryVehicleId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Coordinate = table.Column<string>(type: "TEXT", nullable: false),
+                    DateTime = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeliveryVehicleCoordinates", x => x.DeliveryVehicleCoordinatesId);
+                    table.ForeignKey(
+                        name: "FK_DeliveryVehicleCoordinates_DeliveryVehicles_DeliveryVehicleId",
+                        column: x => x.DeliveryVehicleId,
+                        principalTable: "DeliveryVehicles",
+                        principalColumn: "DeliveryVehicleId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CustomerId = table.Column<int>(type: "INTEGER", nullable: false),
+                    RestaurantId = table.Column<int>(type: "INTEGER", nullable: false),
+                    DeliveryVehicleId = table.Column<int>(type: "INTEGER", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    TrackingNumber = table.Column<string>(type: "TEXT", nullable: false),
+                    SpecialInstructions = table.Column<string>(type: "TEXT", nullable: false),
+                    OrderStatus = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderId);
+                    table.ForeignKey(
+                        name: "FK_Orders_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "CustomerId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Orders_DeliveryVehicles_DeliveryVehicleId",
+                        column: x => x.DeliveryVehicleId,
+                        principalTable: "DeliveryVehicles",
+                        principalColumn: "DeliveryVehicleId");
+                    table.ForeignKey(
+                        name: "FK_Orders_Restaurants_RestaurantId",
+                        column: x => x.RestaurantId,
+                        principalTable: "Restaurants",
+                        principalColumn: "RestaurantId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    RestaurantId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ProductName = table.Column<string>(type: "TEXT", nullable: false),
+                    ProductDescription = table.Column<string>(type: "TEXT", nullable: false),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                    table.ForeignKey(
+                        name: "FK_Products_Restaurants_RestaurantId",
+                        column: x => x.RestaurantId,
+                        principalTable: "Restaurants",
+                        principalColumn: "RestaurantId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderProducts",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ProductId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Ammount = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderProducts", x => new { x.OrderId, x.ProductId });
+                    table.ForeignKey(
+                        name: "FK_OrderProducts_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "OrderId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderProducts_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeliveryVehicleCoordinates_DeliveryVehicleId",
+                table: "DeliveryVehicleCoordinates",
+                column: "DeliveryVehicleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderProducts_OrderId",
+                table: "OrderProducts",
+                column: "OrderId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderProducts_ProductId",
+                table: "OrderProducts",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_CustomerId",
+                table: "Orders",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_DeliveryVehicleId",
+                table: "Orders",
+                column: "DeliveryVehicleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_RestaurantId",
+                table: "Orders",
+                column: "RestaurantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_RestaurantId",
+                table: "Products",
+                column: "RestaurantId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DeliveryVehicleCoordinates");
+
+            migrationBuilder.DropTable(
+                name: "OrderProducts");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "DeliveryVehicles");
+
+            migrationBuilder.DropTable(
+                name: "Restaurants");
+        }
+    }
+}
---
+++ FoodDeliveryAPI/Migrations/20231111221933_AddingInitialDataSet.cs
@@ -0,0 +1,104 @@
+﻿using FoodDeliveryAPI.Model;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodDeliveryAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingInitialDataSet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex("IX_OrderProducts_OrderId");
+            migrationBuilder.DropIndex("IX_OrderProducts_ProductId");
+
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "CustomerId", "CustomerName", "ReceivingAddress", "PhoneNumber" },
+                values: new object[,]
+                {
+                     {1, "Ruben Bernal", "C/ Ribera del Loira, 28. Madrid-28042", "34910502905" },
+                }
+            );
+
+            migrationBuilder.InsertData(
+                table: "DeliveryVehicles",
+                columns: new[] { "DeliveryVehicleId", "DeliverymanName", "DeliverymanPhoneNumber", "VehicleDescription", "Coordinate" },
+                values: new object[,]
+                {
+                    {1, "Jorge Luis Cuellar", "666351395", "Peugeot 508 de Color Blanco", "38.360998544247984, -0.4186471976674082" },
+                }
+            );
+
+            migrationBuilder.InsertData(
+               table: "DeliveryVehicleCoordinates",
+               columns: new[] { "DeliveryVehicleCoordinatesId", "DeliveryVehicleId", "Coordinate", "DateTime" },
+               values: new object[,]
+               {
+                     {1, 1, "38.36356529613985, -0.43049532339588303", new DateTime(2023, 11, 11, 10, 0, 0) },
+                     {2, 1, "38.36379071079803, -0.4254794688681666",  new DateTime(2023, 11, 11, 10, 10, 0) },
+                     {3, 1, "38.36470709244036, -0.4198257059907396",  new DateTime(2023, 11, 11, 10, 20, 0) },
+                     {4, 1, "38.36248906520641, -0.41703969406140096",  new DateTime(2023, 11, 11, 10, 20, 0) },
+                     {5, 1, "38.360998544247984, -0.4186471976674082",  new DateTime(2023, 11, 11, 10, 30, 0) },
+               }
+           );
+
+            migrationBuilder.InsertData(
+                table: "Restaurants",
+                columns: new[] { "RestaurantId", "RestaurantName", "PhoneNumber", "Coordinate", "PickUpAddress" },
+                values: new object[,]
+                {
+                     {1, "Krokante", "966358635", "38.36861684741527, -0.4216071341552255", "Av. Historiador Vicente Ramos, 30, 03540 Alicante" },
+                     {2, "Altramuz", "965466212", "38.369087143477884, -0.4170146457563579",  "Av. de Holanda, 18, 03540 Alicante"},
+                     {3, "Ohana Gastro Bar", "634464540", "38.36841569179011, -0.4273581335542207",  "C. Licia Calderón, 2, 03540 Liria, Alicante"},
+                     {4, "Los Pacos", "965164213", "38.371273785533205, -0.4143527803954427",  "Av. Bruselas, 9, 03540 Alicante"},
+                }
+            );
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "RestaurantId", "ProductName", "ProductDescription", "Price" },
+                values: new object[,]
+                 {
+                     {1, 1, "Empanada argentina", "", 3.20m },
+                     {2, 1, "Chorizo criollo", "", 4.50m },
+                     {3, 1, "Patata bravas", "", 10.80m },
+                     {4, 1, "Huevos rotos con jamón", "", 11.50m },
+                     {5, 2, "Tostas marineras de gambas", "", 8m },
+                     {6, 2, "Capricho Alicantino", "", 10m },
+                     {7, 2, "Berberechos XXL", "", 13m },
+                     {8, 3, "American Burguer", "Deliciosos 200g de carne 100% Black Angus con lechuga, tomate y queso chedar fundido", 10.90m },
+                     {10, 4, "Menú del Día", "Puede elegir un primer plato de entre tres opciones y un segundo de entre otras tres opciones", 25m },
+                }
+            );
+
+            migrationBuilder.InsertData(
+                table: "Orders",
+                columns: new[] { "OrderId", "CustomerId", "RestaurantId", "DeliveryVehicleId", "CreatedDate", "TrackingNumber", "SpecialInstructions", "OrderStatus" },
+                values: new object[,]
+                {
+                     {1, 1, 1, 1, new DateTime(2023, 11, 11, 10, 0, 0), new Guid().ToString(), "Dejar con el portero del edificio", (int)OrderStatus.OnTheWay },
+                }
+            );
+
+            migrationBuilder.InsertData(
+                table: "OrderProducts",
+                columns: new[] { "OrderId", "ProductId", "Ammount" },
+                values: new object[,]
+                {
+                     {1, 1, 2},
+                     {1, 2, 1},
+                     {1, 3, 3},
+                }
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
