3b263c0041473a4d251d23ed1f30f3f549700243
jlcuellar2014 <35740606+jlcuellar2014@users.noreply.github.com>
2023-11-14 21:22:10 +0100

    Created basic unit tests for the OrderService class

--- FoodDeliveryTests/Services/OrdersService_CreateOrderTests.cs
+++ FoodDeliveryTests/Services/OrdersService_CreateOrderTests.cs
@@ -1,4 +1,7 @@
 ﻿using FoodDeliveryAPI.Dtos;
+using FoodDeliveryAPI.Model;
+using FoodDeliveryAPI.Services;
+using FoodDeliveryTests.Model;
 
 namespace FoodDeliveryTests.Services
 {
@@ -9,28 +12,55 @@ namespace FoodDeliveryTests.Services
         public async Task CreateOrderAsync_ValidOrder_CreatesOrderSuccessfully()
         {
             // Arrange
-            using var fakeDbContext = OrdersService_HelperMethods.GetFakeDbContext();
-            var orderService = OrdersService_HelperMethods.GetOrderService(fakeDbContext);
+            using var dbContext = new FakeDbContext();
+            var orderService = new OrdersService(dbContext);
+
+            dbContext.Restaurants.Add(
+                new Restaurant
+                {
+                    RestaurantId = 1,
+                    RestaurantName = "Restaurant",
+                    Coordinate = "1,1",
+                    PhoneNumber = string.Empty,
+                    PickUpAddress = string.Empty
+                });
+
+            dbContext.Products.AddRange(
+                new Product
+                {
+                    ProductId = 1,
+                    RestaurantId = 1,
+                    ProductName = "Product 1 Restauran 1",
+                },
+                new Product
+                {
+                    ProductId = 2,
+                    RestaurantId = 1,
+                    ProductName = "Product 2 Restauran 1",
+                });
+
+            await dbContext.SaveChangesAsync();
+
             var newOrder = new CreateOrderDto
             {
                 CustomerId = 1,
                 RestaurantId = 1,
                 SpecialInstructions = "Special intructions for the order",
                 Products = new List<CreateOrderProductDto>
-            {
-                new CreateOrderProductDto { ProductId = 1, Amount=18,},
-                new CreateOrderProductDto { ProductId = 2, Amount=36,},
-            }
+                {
+                    new CreateOrderProductDto { ProductId = 1, Amount=18,},
+                    new CreateOrderProductDto { ProductId = 2, Amount=36,},
+                }
             };
 
             // Act
             await orderService.CreateOrderAsync(newOrder);
 
             // Assert
-            Assert.IsTrue(fakeDbContext.Orders.Any(o => o.CustomerId.Equals(1) && o.RestaurantId.Equals(1)));
+            Assert.IsTrue(dbContext.Orders.Any(o => o.CustomerId.Equals(1) && o.RestaurantId.Equals(1)));
 
             var productIdsToCheck = new int[] { 1, 2 };
-            var count = fakeDbContext.OrderProducts.Count(op => productIdsToCheck.Contains(op.ProductId));
+            var count = dbContext.OrderProducts.Count(op => productIdsToCheck.Contains(op.ProductId));
 
             Assert.That(count, Is.EqualTo(2));
         }
@@ -39,8 +69,8 @@ namespace FoodDeliveryTests.Services
         public void CreateOrderAsync_NullOrder_ThrowsArgumentNullException()
         {
             // Arrange
-            using var fakeDbContext = OrdersService_HelperMethods.GetFakeDbContext();
-            var orderService = OrdersService_HelperMethods.GetOrderService(fakeDbContext);
+            using var dbContext = new FakeDbContext();
+            var orderService = new OrdersService(dbContext);
 
             // Act & Assert
             Assert.ThrowsAsync<ArgumentNullException>(async () => await orderService.CreateOrderAsync(null));
@@ -50,8 +80,9 @@ namespace FoodDeliveryTests.Services
         public void CreateOrderAsync_NullProducts_ThrowsArgumentNullException()
         {
             // Arrange
-            using var fakeDbContext = OrdersService_HelperMethods.GetFakeDbContext();
-            var orderService = OrdersService_HelperMethods.GetOrderService(fakeDbContext);
+            using var dbContext = new FakeDbContext();
+            var orderService = new OrdersService(dbContext);
+
             CreateOrderDto newOrder = new() { CustomerId = 1, RestaurantId = 1, Products = null };
 
             // Act & Assert
@@ -59,11 +90,32 @@ namespace FoodDeliveryTests.Services
         }
 
         [Test]
-        public void CreateOrderAsync_ProductsNotBelongingToRestaurant_ThrowsArgumentException()
+        public async Task CreateOrderAsync_ProductsNotBelongingToRestaurant_ThrowsArgumentExceptionAsync()
         {
             // Arrange
-            using var fakeDbContext = OrdersService_HelperMethods.GetFakeDbContext();
-            var orderService = OrdersService_HelperMethods.GetOrderService(fakeDbContext);
+            using var dbContext = new FakeDbContext();
+            var orderService = new OrdersService(dbContext);
+
+            dbContext.Restaurants.Add(
+               new Restaurant
+               {
+                   RestaurantId = 1,
+                   RestaurantName = "Restaurant",
+                   Coordinate = "1,1",
+                   PhoneNumber = string.Empty,
+                   PickUpAddress = string.Empty
+               });
+
+            dbContext.Products.Add(
+                new Product
+                {
+                    ProductId = 1,
+                    RestaurantId = 2,
+                    ProductName = "Product 1 Restauran 2"
+                });
+
+            await dbContext.SaveChangesAsync();
+
             var newOrder = new CreateOrderDto
             {
                 CustomerId = 1,
---
+++ FoodDeliveryTests/Services/OrdersService_GetOrderCoordinateTests.cs
@@ -0,0 +1,121 @@
+﻿using FoodDeliveryAPI.Model;
+using FoodDeliveryAPI.Services;
+using FoodDeliveryTests.Model;
+using FoodDeliveryTests.Utilities;
+
+namespace FoodDeliveryTests.Services
+{
+    [TestFixture]
+    public class OrdersService_GetOrderCoordinateTests
+    {
+        [Test]
+        public async Task GetOrderCoordinateAsync_OrderExists_ReturnsCoordinateDto()
+        {
+            // Arrange
+            using var dbContext = new FakeDbContext();
+            var orderService = new OrdersService(dbContext);
+
+            dbContext.Orders.Add(
+                new Order
+                {
+                    TrackingNumber = "123",
+                    OrderStatus = OrderStatus.Pending,
+                    RestaurantId = 1
+                });
+
+            dbContext.Restaurants.Add(
+                new Restaurant
+                {
+                    RestaurantId = 1,
+                    RestaurantName = "Restaurant",
+                    Coordinate = "1,1",
+                    PhoneNumber = string.Empty,
+                    PickUpAddress = string.Empty
+                });
+
+            await dbContext.SaveChangesAsync();
+
+            // Act
+            var coordinate = await orderService.GetOrderCoordinateAsync("123");
+
+            // Assert
+            Assert.IsNotNull(coordinate);
+            Assert.That(coordinate.ToPlainFormat(), Is.EqualTo("1,1"));
+        }
+
+        [Test]
+        public async Task GetOrderCoordinateAsync_OrderOnTheWayWithVehicle_ReturnsCoordinateDto()
+        {
+            // Arrange
+            using var dbContext = new FakeDbContext();
+            var orderService = new OrdersService(dbContext);
+
+            dbContext.Orders.Add(
+                new Order
+                {
+                    TrackingNumber = "123",
+                    OrderStatus = OrderStatus.OnTheWay,
+                    RestaurantId = 1,
+                    DeliveryVehicleId = 1
+                });
+
+            dbContext.DeliveryVehicles.Add(
+                new DeliveryVehicle
+                {
+                    DeliveryVehicleId = 1,
+                    Coordinate = "1,1",
+                    DeliverymanName = "Delivery",
+                    DeliverymanPhoneNumber = string.Empty,
+                    VehicleDescription = string.Empty,
+                });
+
+            await dbContext.SaveChangesAsync();
+
+            // Act
+            var coordinate = await orderService.GetOrderCoordinateAsync("123");
+
+            // Assert
+            Assert.IsNotNull(coordinate);
+            Assert.That(coordinate.ToPlainFormat(), Is.EqualTo("1,1"));
+        }
+
+        [Test]
+        public async Task GetOrderCoordinateAsync_OrderDelivered_ThrowsException()
+        {
+            // Arrange
+            using var dbContext = new FakeDbContext();
+            var orderService = new OrdersService(dbContext);
+
+            dbContext.Orders.Add(
+                new Order { 
+                    TrackingNumber = "456", 
+                    OrderStatus = OrderStatus.Delivered
+                });
+
+            await dbContext.SaveChangesAsync();
+
+            // Act & Assert
+            Assert.ThrowsAsync<Exception>(() => orderService.GetOrderCoordinateAsync("456"));
+        }
+
+        [Test]
+        public async Task GetOrderCoordinateAsync_OrderOnTheWayWithoutVehicle_ThrowsException()
+        {
+            // Arrange
+            using var dbContext = new FakeDbContext();
+            var orderService = new OrdersService(dbContext);
+
+            dbContext.Orders.Add(
+                new Order { 
+                    TrackingNumber = "101", 
+                    OrderStatus = OrderStatus.OnTheWay, 
+                    DeliveryVehicleId = null 
+                });
+
+            await dbContext.SaveChangesAsync();
+
+            // Act & Assert
+            Assert.ThrowsAsync<Exception>(() => orderService.GetOrderCoordinateAsync("101"));
+        }
+    }
+}
