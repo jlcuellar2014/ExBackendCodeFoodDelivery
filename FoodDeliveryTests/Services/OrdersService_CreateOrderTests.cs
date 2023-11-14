ea9058706ed408deb7499b60d407044f8b7c80cc
jlcuellar2014 <35740606+jlcuellar2014@users.noreply.github.com>
2023-11-14 20:11:14 +0100

    I separated the responsibility of creating test data for the tests from the OrderService class to an external class

---
+++ FoodDeliveryTests/Services/OrdersService_CreateOrderTests.cs
@@ -0,0 +1,84 @@
+﻿using FoodDeliveryAPI.Dtos;
+
+namespace FoodDeliveryTests.Services
+{
+    [TestFixture]
+    public class OrdersService_CreateOrderTests
+    {
+        [Test]
+        public async Task CreateOrderAsync_ValidOrder_CreatesOrderSuccessfully()
+        {
+            // Arrange
+            using var fakeDbContext = OrdersService_HelperMethods.GetFakeDbContext();
+            var orderService = OrdersService_HelperMethods.GetOrderService(fakeDbContext);
+            var newOrder = new CreateOrderDto
+            {
+                CustomerId = 1,
+                RestaurantId = 1,
+                SpecialInstructions = "Special intructions for the order",
+                Products = new List<CreateOrderProductDto>
+            {
+                new CreateOrderProductDto { ProductId = 1, Amount=18,},
+                new CreateOrderProductDto { ProductId = 2, Amount=36,},
+            }
+            };
+
+            // Act
+            await orderService.CreateOrderAsync(newOrder);
+
+            // Assert
+            Assert.IsTrue(fakeDbContext.Orders.Any(o => o.CustomerId.Equals(1) && o.RestaurantId.Equals(1)));
+
+            var productIdsToCheck = new int[] { 1, 2 };
+            var count = fakeDbContext.OrderProducts.Count(op => productIdsToCheck.Contains(op.ProductId));
+
+            Assert.That(count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void CreateOrderAsync_NullOrder_ThrowsArgumentNullException()
+        {
+            // Arrange
+            using var fakeDbContext = OrdersService_HelperMethods.GetFakeDbContext();
+            var orderService = OrdersService_HelperMethods.GetOrderService(fakeDbContext);
+
+            // Act & Assert
+            Assert.ThrowsAsync<ArgumentNullException>(async () => await orderService.CreateOrderAsync(null));
+        }
+
+        [Test]
+        public void CreateOrderAsync_NullProducts_ThrowsArgumentNullException()
+        {
+            // Arrange
+            using var fakeDbContext = OrdersService_HelperMethods.GetFakeDbContext();
+            var orderService = OrdersService_HelperMethods.GetOrderService(fakeDbContext);
+            CreateOrderDto newOrder = new() { CustomerId = 1, RestaurantId = 1, Products = null };
+
+            // Act & Assert
+            Assert.ThrowsAsync<ArgumentNullException>(async () => await orderService.CreateOrderAsync(newOrder));
+        }
+
+        [Test]
+        public void CreateOrderAsync_ProductsNotBelongingToRestaurant_ThrowsArgumentException()
+        {
+            // Arrange
+            using var fakeDbContext = OrdersService_HelperMethods.GetFakeDbContext();
+            var orderService = OrdersService_HelperMethods.GetOrderService(fakeDbContext);
+            var newOrder = new CreateOrderDto
+            {
+                CustomerId = 1,
+                RestaurantId = 1,
+                SpecialInstructions = "Special intructions for the order",
+                Products = new List<CreateOrderProductDto>
+            {
+                new CreateOrderProductDto { ProductId = 3, Amount=1,}
+            }
+            };
+
+            // Act & Assert
+            var exception = Assert.ThrowsAsync<ArgumentException>(async () => await orderService.CreateOrderAsync(newOrder));
+            Assert.That(exception.Message, Is.EqualTo("Some products in the order, do not correspond to the restaurant used in the order"));
+        }
+    }
+
+}
---
+++ FoodDeliveryTests/Services/OrdersService_HelperMethods.cs
@@ -0,0 +1,71 @@
+﻿using FoodDeliveryAPI.Model;
+using FoodDeliveryAPI.Services;
+using FoodDeliveryTests.Model;
+
+namespace FoodDeliveryTests.Services
+{
+    public static class OrdersService_HelperMethods
+    {
+        public static FakeDbContext GetFakeDbContext()
+        {
+            var fakeDbContext = new FakeDbContext();
+
+            fakeDbContext.Customers.Add(
+                new Customer
+                {
+                    CustomerId = 1,
+                    CustomerName = "Jorge",
+                    PhoneNumber = "666351395",
+                    ReceivingAddress = "Alicante"
+                });
+
+            fakeDbContext.Restaurants.AddRange(
+                new Restaurant
+                {
+                    RestaurantId = 1,
+                    RestaurantName = "Restaurant 1",
+                    Coordinate = "1,1",
+                    PhoneNumber = string.Empty,
+                    PickUpAddress = string.Empty
+                },
+                new Restaurant
+                {
+                    RestaurantId = 2,
+                    RestaurantName = "Restaurant 2",
+                    Coordinate = "2,2",
+                    PhoneNumber = string.Empty,
+                    PickUpAddress = string.Empty
+                });
+
+            fakeDbContext.Products.AddRange(
+                new Product
+                {
+                    ProductId = 1,
+                    RestaurantId = 1,
+                    ProductName = "Product 1 Restauran 1",
+                },
+                new Product
+                {
+                    ProductId = 2,
+                    RestaurantId = 1,
+                    ProductName = "Product 2 Restauran 1",
+                },
+                new Product
+                {
+                    ProductId = 3,
+                    RestaurantId = 2,
+                    ProductName = "Product 3 Restauran 2",
+                }
+            );
+
+            fakeDbContext.SaveChanges();
+
+            return fakeDbContext;
+        }
+
+        public static IOrdersService GetOrderService(IFoodDeliveryContext dbContext)
+        {
+            return new OrdersService(dbContext);
+        }
+    }
+}
