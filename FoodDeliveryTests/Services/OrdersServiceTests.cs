a5100c9dabe0c87a382f1ccb7d184b65434f4d5d
jlcuellar2014 <35740606+jlcuellar2014@users.noreply.github.com>
2023-11-14 13:42:11 +0100

    Some tests for null parameters and data inconsistency were made

--- FoodDeliveryTests/Services/OrdersServiceTests.cs
+++ FoodDeliveryTests/Services/OrdersServiceTests.cs
@@ -1,33 +1,73 @@
-﻿using NUnit.Framework;
-using FoodDeliveryAPI.Services;
+﻿using FoodDeliveryAPI.Model;
+using FoodDeliveryAPI.Dtos;
+using FoodDeliveryTests.Model;
 
 namespace FoodDeliveryAPI.Services.Tests
 {
     [TestFixture()]
     public class OrdersServiceTests
     {
-        [Test()]
-        public void CreateOrderAsyncTest()
+        [Test]
+        public void CreateOrder_NullNewOrder_ThrowArgumentNullException()
         {
-            Assert.Fail();
+            // Arrange
+            var fakeDbContext = new FakeDbContext();
+            var ordersService = new OrdersService(fakeDbContext);
+            CreateOrderDto newOrder = null;
+
+            // Act y Assert
+            Assert.ThrowsAsync<ArgumentNullException>(() => ordersService.CreateOrderAsync(newOrder));
         }
 
-        [Test()]
-        public void OrdersServiceTest()
+        [Test]
+        public void CreateOrder_NullListProductsInOrden_ThrowArgumentNullException()
         {
-            Assert.Fail();
+            // Arrange
+            var fakeDbContext = new FakeDbContext();
+            var ordersService = new OrdersService(fakeDbContext);
+            CreateOrderDto newOrder = new() { CustomerId = 1, RestaurantId = 1, Products = null };
+
+            // Act y Assert
+            Assert.ThrowsAsync<ArgumentNullException>(() => ordersService.CreateOrderAsync(newOrder));
         }
 
-        [Test()]
-        public void CreateOrderAsyncTest1()
+        [Test]
+        public void CreateOrder_ProductsWithWrongRestaurantId_ThrowArgumentException()
         {
-            Assert.Fail();
-        }
+            // Arrange
+            var fakeDbContext = new FakeDbContext();
 
-        [Test()]
-        public void GetOrderCoordinateAsyncTest()
-        {
-            Assert.Fail();
+            fakeDbContext.Restaurants.Add(new Restaurant
+            {
+                RestaurantId = 1,
+                RestaurantName = "Restaurant",
+                Coordinate = string.Empty,
+                PhoneNumber = string.Empty,
+                PickUpAddress = string.Empty
+            });
+
+            fakeDbContext.Products.Add(new Product
+            {
+                ProductId = 1,
+                RestaurantId = 2, // This product not correspond to the restaurant
+                ProductName = "Product",
+            });
+
+            fakeDbContext.SaveChanges();
+
+            var ordersService = new OrdersService(fakeDbContext);
+
+            var newOrder = new CreateOrderDto
+            {
+                CustomerId = 1,
+                RestaurantId = 1,
+                Products = new List<CreateOrderProductDto> {
+                    new CreateOrderProductDto{ProductId = 1}
+                }
+            };
+
+            // Act y Assert
+            Assert.ThrowsAsync<ArgumentException>(() => ordersService.CreateOrderAsync(newOrder));
         }
     }
 }
