814a0fd6cda219c3f3a459de64fe45dc28d8b7eb
jlcuellar2014 <35740606+jlcuellar2014@users.noreply.github.com>
2023-11-14 23:52:26 +0100

    Unit tests for DeliveryVehicleService class were made

---
+++ FoodDeliveryTests/Services/DeliveryVehiclesService_GetDeliveryVehicleCoordinateTests.cs
@@ -0,0 +1,49 @@
+﻿using FoodDeliveryAPI.Model;
+using FoodDeliveryAPI.Services;
+using FoodDeliveryTests.Model;
+using FoodDeliveryTests.Utilities;
+
+namespace FoodDeliveryTests.Services
+{
+
+    [TestFixture]
+    public class DeliveryVehiclesService_GetDeliveryVehicleCoordinateTests
+    {
+        [Test]
+        public async Task GetDeliveryVehicleCoordinateAsync_ValidId_ReturnsCoordinateDto()
+        {
+            // Arrange
+            using var dbContext = new FakeDbContext();
+            var deliveryService = new DeliveryVehiclesService(dbContext);
+
+            var vehicle = new DeliveryVehicle { 
+                DeliveryVehicleId = 1,
+                Coordinate = "1,1",
+                DeliverymanName=string.Empty,
+                DeliverymanPhoneNumber=string.Empty,
+                VehicleDescription=string.Empty
+            };
+
+            dbContext.DeliveryVehicles.Add(vehicle);
+            await dbContext.SaveChangesAsync();
+
+            // Act
+            var coordinates = await deliveryService.GetDeliveryVehicleCoordinateAsync(1);
+
+            // Assert
+            Assert.IsNotNull(coordinates);
+            Assert.That(coordinates.ToPlainFormat(), Is.EqualTo("1,1"));
+        }
+
+        [Test]
+        public void GetDeliveryVehicleCoordinateAsync_InvalidId_ThrowsException()
+        {
+            // Arrange
+            using var dbContext = new FakeDbContext();
+            var deliveryService = new DeliveryVehiclesService(dbContext);
+
+            // Act & Assert
+            Assert.ThrowsAsync<ArgumentException>(() => deliveryService.GetDeliveryVehicleCoordinateAsync(1));
+        }
+    }
+}
---
+++ FoodDeliveryTests/Services/DeliveryVehiclesService_UpdateDeliveryVehicleCoordinateTests.cs
@@ -0,0 +1,59 @@
+﻿using FoodDeliveryAPI.Dtos;
+using FoodDeliveryAPI.Model;
+using FoodDeliveryAPI.Services;
+using FoodDeliveryTests.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodDeliveryTests.Services
+{
+    [TestFixture]
+    public class DeliveryVehiclesService_UpdateDeliveryVehicleCoordinateTests
+    {
+        [Test]
+        public async Task UpdateDeliveryVehicleCoordinateAsync_ValidId_UpdatesCoordinateAndAddsHistorical()
+        {
+            // Arrange
+            using var dbContext = new FakeDbContext();
+            var deliveryService = new DeliveryVehiclesService(dbContext);
+
+            var vehicle = new DeliveryVehicle { 
+                DeliveryVehicleId = 1, 
+                Coordinate = "0,0",
+                DeliverymanName = string.Empty,
+                DeliverymanPhoneNumber = string.Empty,
+                VehicleDescription = string.Empty,
+            };
+
+            dbContext.DeliveryVehicles.Add(vehicle);
+            await dbContext.SaveChangesAsync();
+
+            var newCoordinate = new CoordinateDto { Latitude = 1, Longitude = 1 };
+
+            // Act
+            await deliveryService.UpdateDeliveryVehicleCoordinateAsync(1, newCoordinate);
+
+            // Assert
+            var updatedVehicle = await dbContext.DeliveryVehicles.FindAsync(1);
+            var historicalCoordinate = await dbContext.DeliveryVehicleCoordinates.FirstOrDefaultAsync();
+
+            Assert.That(updatedVehicle, Is.Not.Null);
+            Assert.That(updatedVehicle.Coordinate, Is.EqualTo("1,1"));
+
+            Assert.IsNotNull(historicalCoordinate);
+            Assert.That(historicalCoordinate.Coordinate, Is.EqualTo("1,1"));
+        }
+
+        [Test]
+        public void UpdateDeliveryVehicleCoordinateAsync_InvalidId_ThrowsException()
+        {
+            // Arrange
+            using var dbContext = new FakeDbContext();
+            var deliveryService = new DeliveryVehiclesService(dbContext);
+
+            var newCoordinate = new CoordinateDto { Latitude = 1, Longitude = 1 };
+
+            // Act & Assert
+            Assert.ThrowsAsync<Exception>(() => deliveryService.UpdateDeliveryVehicleCoordinateAsync(1, newCoordinate));
+        }
+    }
+}
