bb3652d9605b763fd35e3133592ee4f4b2cd9258
jlcuellar2014 <35740606+jlcuellar2014@users.noreply.github.com>
2023-11-14 02:10:42 +0100

    Initial schema made

---
+++ FoodDeliveryTests/Services/OrdersServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using FoodDeliveryAPI.Services;
+
+namespace FoodDeliveryAPI.Services.Tests
+{
+    [TestFixture()]
+    public class OrdersServiceTests
+    {
+        [Test()]
+        public void CreateOrderAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void OrdersServiceTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void CreateOrderAsyncTest1()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void GetOrderCoordinateAsyncTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
